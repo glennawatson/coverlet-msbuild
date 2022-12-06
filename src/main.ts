d5b5a40564d8894592764db74791d36d3858323d
David Vreony <dpvreony@users.noreply.github.com>
2022-12-06 10:47:35 +0000

    move project arg to be 2nd arg

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function run(): Promise<void> {
 
     const globber = await glob.create(Inputs.projectFiles, globOptions);
 
-    const defaultArgs = ['test', '/p:CollectCoverage=true'];
+    const defaultArgs = ['/p:CollectCoverage=true'];
 
     if (Inputs.noBuild) {
       defaultArgs.push('--no-build');
@@ -117,7 +117,8 @@ async function run(): Promise<void> {
         }
       }
 
-      runArgs.push(currentFile);
+      runArgs.unshift(currentFile);
+      runArgs.unshift('test');
 
       await exec('dotnet', runArgs, {
         cwd: workingDirectory
